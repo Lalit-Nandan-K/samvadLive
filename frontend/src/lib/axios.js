db7f13a02bc836be0f028e83a5b73f1792bf9982
Lalit <lalitnandan19@gmail.com>
2025-11-19 21:58:06 +0530

    axios

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" 
-  ? "http://localhost:5001/api" 
-  : "/api";
+const BASE_URL = import.meta.env.MODE === "development"
+  ? "http://localhost:5001/api"
+  : import.meta.env.VITE_BACKEND_URL + "/api";
+
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
